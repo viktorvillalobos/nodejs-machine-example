ada06e592e690ab6b8133faa56c282255f0467bc
Víktor Villalobos <victorvillalobos@outlook.com>
2021-10-16 00:52:00 -0300

    chore(): change some things

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ COPY . .
 RUN npm run build
 
 
-CMD [ "node", "./dist/server.js" ]
+CMD [ "node", "./dist/main.js" ]
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ const port = 3000
 const pool = createNewPostgreSQLPool()
 const app = createNewHttpServer(pool)
 
+// This should use a Logger instad of console.log
+
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Rendalo APP Running at http://localhost:${port}`)
